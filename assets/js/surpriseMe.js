9294928b54d437b11dc5e7d55738421d8963dba0
Pushkar Sharma <96145925+Push4ck@users.noreply.github.com>
2023-12-26 17:59:30 +0530

    Add files via upload

---
+++ assets/js/surpriseMe.js
@@ -0,0 +1,99 @@
+// Array containing surprise prompts for the "Surprise me" button
+const surpriseMe = [
+    {
+        no: "1",
+        Text: "abstract pencil and watercolor art of a lonely robot holding a balloon"
+    },
+    {
+        no: "2",
+        Text: "an astronaut lounging in a tropical resort in space, pixel art"
+    },
+    {
+        no: "3",
+        Text: "a stained glass window depicting a robot"
+    },
+    {
+        no: "4",
+        Text: "a stained glass window depicting a hamburger and french fries"
+    },
+    {
+        no: "5",
+        Text: "photograph of an astronaut riding a horse"
+    },
+    {
+        no: "6",
+        Text: "a sunlit indoor lounge area with a pool with clear water and another pool with translucent pastel pink water, next to a big window, digital art"
+    },
+    {
+        no: "7",
+        Text: "3D render of a small pink balloon dog in a light pink room"
+    },
+    {
+        no: "8",
+        Text: "a fortune-telling shiba inu reading your fate in a giant hamburger, digital art"
+    },
+    {
+        no: "9",
+        Text: "an expressive oil painting of a basketball player dunking, depicted as an explosion of a nebula"
+    },
+    {
+        no: "10",
+        Text: "a pencil and watercolor drawing of a bright city in the future with flying cars"
+    },
+    {
+        no: "11",
+        Text: "a cat submarine chimera, digital art"
+    },
+    {
+        no: "12",
+        Text: "a surrealist dream-like oil painting by Salvador Dalí of a cat playing checkers"
+    },
+    {
+        no: "13",
+        Text: "an ethereal digital painting of a cyborg dancing under a neon-lit sky"
+    },
+    {
+        no: "14",
+        Text: "a surreal pixel art representation of a mermaid riding a hoverboard over pixelated waves"
+    },
+    {
+        no: "15",
+        Text: "a cosmic kaleidoscope of a robotic hummingbird sipping nectar from a floating pixel flower"
+    },
+    {
+        no: "16",
+        Text: "a whimsical scene of a steampunk octopus playing jazz on a vinyl record, rendered in watercolor"
+    },
+    {
+        no: "17",
+        Text: "a fantastical sci-fi cityscape with towering mushroom buildings and flying dragon-like hovercraft, captured in oil on canvas"
+    },
+    {
+        no: "18",
+        Text: "a futuristic metropolis where giant robotic butterflies roam freely among skyscrapers, depicted in vibrant pastel colors"
+    },
+    {
+        no: "19",
+        Text: "a dreamy digital collage of a bookshelf in outer space, where each book contains a universe of its own"
+    },
+    {
+        no: "20",
+        Text: "an otherworldly scene of a cosmic fox riding a comet through a galaxy of swirling colors, created with digital brush strokes"
+    },
+    {
+        no: "21",
+        Text: "a mesmerizing 3D render of a floating crystal palace inhabited by luminescent jellyfish-like creatures"
+    },
+    {
+        no: "22",
+        Text: "a magical forest scene where neon fireflies illuminate the path for a robotic deer, portrayed in a mix of watercolor and digital art"
+    },
+    {
+        no: "23",
+        Text: "an avant-garde representation of a cyberpunk cityscape with glitched-out skyscrapers and futuristic graffiti, crafted in mixed media"
+    },
+    {
+        no: "24",
+        Text: "a mind-bending digital interpretation of a robotic phoenix rising from glitched pixel ashes against a backdrop of circuitry"
+    }
+];
